96f36db55f6467dbd05a8f90d7865cc18086646b
ropeJIbI4 <pogi199411@gmail.com>
2024-02-20 23:44:51 +0300

    commit

---
+++ data.js
@@ -0,0 +1,5 @@
+const data ={
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "id": "1jP2VyNFQPemJB71GePV_g",
+        "url": "",
+        "value": "Peter Piper picked a peck of pickled peppers. Chuck Norris shot him and screwed his mother against a wall for three hours."}
